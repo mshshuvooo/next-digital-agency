51cb594b8c4949e96b42a8487180e965069cd78d
Shuvo <shuvombm02@gmail.com>
2021-07-30 20:28:25 +0600

    Achievements counter up added

--- src/components/achievements/Achievements.js
+++ src/components/achievements/Achievements.js
@@ -6,14 +6,16 @@ const Achievements = (props) => {
     console.log(achievementLists);
     return (
         <div className="achievement-items">
-            <CountUp start={0} end={120} />
 
             {achievementLists && achievementLists?.map( (item) => {
                 return(
                     <div key={Math.random()} className="single-achievement-item">
                         <div className="achievement-item-inner">
                             <i className={item.achievementIcon}></i>
-                            <h3><CountUp end={item.achievementNumber} />{item.achievementNumberExtra && <span>{item.achievementNumberExtra}</span>}</h3>
+                            <h3>
+                                <CountUp start={0} end={item.achievementNumber} duration={2.75}/>
+                                {item.achievementNumberExtra && <span>{item.achievementNumberExtra}</span>}
+                            </h3>
                             <h4>{item.achievementTitle}</h4>
                         </div>
                     </div>
